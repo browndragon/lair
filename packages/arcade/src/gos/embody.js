de35f61f83a5e26db1f07a1e7dcebb41bc46dbfd
Brown Dragon <browndragon@googlegroups.com>
2021-05-05 16:01:20 -0400

    tune default embodies

--- packages/arcade/src/gos/embody.js
+++ packages/arcade/src/gos/embody.js
@@ -3,7 +3,7 @@
  * This is horribly stupid but also true.
  * So: instead, override your addedToScene at the leaf to call bouncy(this).
  */
-export default function embody(object, {bounce=1, drag=1024, mass=1, useDamping=false}={}) {
+export default function embody(object, {bounce=1/64, drag=1024, mass=1, useDamping=false}={}) {
     object.scene.physics.world.enable(object);  // Just in case it hasn't already happened.
     // object.body.setCollideWorldBounds(true, bounce, bounce);
     // object.body.onWorldBounds = true;
